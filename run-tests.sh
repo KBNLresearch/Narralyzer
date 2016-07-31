2c91e3934b1802b4b00a92d1d0d7ed397fd08acb
Willem Jan Faber <willemjan.faber@fe2.nl>
2016-07-31 16:05:21 +0200

    sleepit

--- run-tests.sh
+++ run-tests.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
-
-timeout 1 bash -c 'cat < /dev/null > /dev/tcp/localhost/9991'
-while [ $? == "1" ]; do
-    timeout 1 bash -c 'cat < /dev/null > /dev/tcp/localhost/9991'
-    sleep 1
-    echo "Stanford not ready, waiting to boot.."
-done
-
-
-
-timeout 1 bash -c 'cat < /dev/null > /dev/tcp/localhost/9990'
-while [ $? == "1" ]; do
-    timeout 1 bash -c 'cat < /dev/null > /dev/tcp/localhost/9990'
-    sleep 1
-    echo "Stanford not ready, waiting to boot.."
-done
-
 . start_stanford.sh
+sleep 60
 . env/bin/activate
 python2.7 ./narralyzer/lang_lib.py test
 python2.7 ./narralyzer/stanford_ner_wrapper.py test
