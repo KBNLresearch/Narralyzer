316ac5f6c788c126a052cd41a86b98cebef40a3e
Willem Jan Faber <willemjan.faber@fe2.nl>
2016-07-31 16:21:10 +0200

    Wait for Stanford to boot normally

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+
+# This file is part of narralyzer
+# http://www.narralyzer.com/
+
 . start_stanford.sh
-sleep 60
+while true; do nc -w 1 127.0.0.1 9991 && break; sleep 1; echo "."; done
+while true; do nc -w 1 127.0.0.1 9992 && break; sleep 1; echo "."; done
 . env/bin/activate
-python2.7 ./narralyzer/lang_lib.py test
-python2.7 ./narralyzer/stanford_ner_wrapper.py test
+python2.7 ./narralyzer/lang_lib.py test || exit -
+python2.7 ./narralyzer/stanford_ner_wrapper.py test || exit -
