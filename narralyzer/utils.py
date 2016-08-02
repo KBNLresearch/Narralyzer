3ddc14e9ae3fe5de1a5218f76a8e8ebead3dc0f4
WillemJan Faber <wjfprive@xs4all.nl>
2016-08-02 09:37:49 +0200

    Update utils.py

--- narralyzer/utils.py
+++ narralyzer/utils.py
@@ -24,10 +24,10 @@ from lang_lib import Language
 current_path = os.path.realpath(__file__.replace(os.path.basename(os.path.realpath(__file__)), os.sep))
 
 # Path to test data
-TEST_DATA = os.path.join(current_path, '..', 'test_data')
+TEST_DATA = os.path.join(current_path, 'test_data')
 
 # Path to put output files (parsed / zipped books)
-OUTPUT = os.path.join(current_path, '..', 'out')
+OUTPUT = os.path.join(current_path, 'out')
 
 
 def load_test_book(fname, force=True, return_json=False, verbose=True):
